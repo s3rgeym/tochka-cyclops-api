8a7bbc88c8a7c08f76e8b0cb012ad5606c288ed4
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2023-10-21 19:49:05 +0300

    декомпозиция

---
+++ tochka_cyclops_api/utils.py
@@ -0,0 +1,12 @@
+import re
+
+
+def camel_to_snake(name: str) -> str:
+    name = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", name)
+    return re.sub("([a-z0-9])([A-Z])", r"\1_\2", name).lower()
+
+
+class AttrDict(dict):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self.__dict__ = self
