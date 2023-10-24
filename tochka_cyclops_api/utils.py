f49834109a27e96997bf8c75665bd791f231c58f
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2023-10-24 03:51:31 +0300

    AttrDict.from_dict

--- tochka_cyclops_api/utils.py
+++ tochka_cyclops_api/utils.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
+import collections
 import re
-from typing import Any
+from typing import Any, Type
 
 
 def camel_to_snake(name: str) -> str:
@@ -11,3 +14,18 @@ class AttrDict(dict):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
         self.__dict__ = self
+
+    @classmethod
+    def _from_value(cls: Type[AttrDict], v: Any) -> Any:
+        return (
+            cls.from_dict(v)
+            if isinstance(v, dict)
+            else list(map(cls._from_value, v))
+            if isinstance(v, collections.abc.Sequence)
+            and not isinstance(v, (str, bytes))
+            else v
+        )
+
+    @classmethod
+    def from_dict(cls: Type[AttrDict], d: dict) -> AttrDict:
+        return cls({k: cls._from_value(v) for k, v in d.items()})
